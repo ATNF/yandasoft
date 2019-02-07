0e9373850092f9db5e21ece9cf13860153ab5b1d
StephenOrd <StephenOrd@csiro.au>
2019-02-07 03:34:24 +0000

    ASKAPSDP-3380 This is the new group based tree reduction for reduceNE. I have cheked and it produces identical answers so I'm putting it in now. This will permit some larger scale testing

--- synthesis/current/parallel/MEParallel.cc
+++ synthesis/current/parallel/MEParallel.cc
@@ -116,7 +116,7 @@ void MEParallel::receiveNE()
 }
 
 /*
- * This method performs a graph reduction (using a binary tree topology)
+ * The original method performed a graph reduction (using a binary tree topology)
  * from all processes to rank zero. The sequence of workers is mapped to
  * a binary tree like so:
  * - Rank 0 is the root
@@ -138,20 +138,86 @@ void MEParallel::receiveNE()
  *
  * In the above case the result is a perfect binary tree (all leaves are at the
  * same depth) however this method will also handle the imperfect case.
- */ 
+ *
+ * 2019 changed the tree model - Now we have:
+ * nGroup trees where nGroup can be 1
+ * - the number of ranks in each tree is NProcPerGroup =  nProcs-1/NGroups
+ * - the root of each is group*NProcPerGroup + 1
+ * - the rankInGroup is rank - group*NProcPerGroup
+ * - to find the parent of a rank floor(rankInGroup / 2)
+ * - to find the left child of a node: (2 * rankInGroup)
+ * - to find the right child of a node: (2 * rankInGroup) + 1
+ * - For example - 22 ranks, 1 master (0) 3 Groups each with 7 members
+ *
+ * - Looking at group 0
+ * - root = 1
+ * - parent(1) = 0
+ * - left(1) = 2
+ * - right(1) = 3
+ * - parent(2) = 1
+ * - left(2) = 4
+ * - right(2) = 5
+ * - parent(3) = 1
+ * - left(3) = 6;
+ * - right(3) = 7
+ * - Looking at group 1
+ * - root = 1*7 + 1 = 8
+ * but rankIngroup = 8 - 1*7 = 1
+ * - parent (1) repeat group 0
+ * but the remember send and receives have to use absolute ranks
+ */
 void MEParallel::reduceNE(askap::scimath::INormalEquations::ShPtr ne)
 {
+    // I have changed this tree to have nGroup branches. In other words this is
+    // nGroup binary trees
+
     // Number of processes in the reduction
-    const int nProcs = itsComms.nProcs();
+    int nProcs = itsComms.nProcs();
+    int nProcsPerGroup = nProcs;
 
     // Rank (zero-based)
-    const int rank = itsComms.rank();
+    int rank = itsComms.rank();
 
+    // nGroups (one-based)
+    int nGroups = itsComms.nGroups();
+
+    if (nGroups > 1)
+      nProcsPerGroup = (nProcs-1)/nGroups;
+
+    if (itsComms.isMaster()) {
+
+      // previously you could just go through the loop logic as any rank and you would
+      // end up receiving from ranks 1 and 2 as the last row of the reduction.
+      // With this new group based reduction that does not work and you need
+      // the case for the Master made explicit.
+      // THe master now has to receive from nGroup senders.
+      for (int theGroup=0; theGroup < nGroups; theGroup++) {
+        ne->merge(*receiveNormalEquations(theGroup*nProcsPerGroup+1));
+      }
+      return;
+    }
+
+    // Group (zero-based)
+    int Group = itsComms.group();
+
+    if (nGroups > 0) {
+
+
+      rank = rank - Group*nProcsPerGroup;
+
+      // example: 7 proc - 3 groups + Master
+      // nProcsPerGroup = (7-1)/3 = 2
+      // rank 1 is group 0 so:
+      // rank = 1 - 0
+      // rank 5 is group 2 so:
+      // rank = 5 - 2*2 = 1 ...
+
+    }
     // This is the height of the binary tree. For example:
     // floor(log2(1)) == 0
     // floor(log2(3)) == 1
     // floor(log2(4)) == 2
-    const int height = int(floor(log2(nProcs)));
+    const int height = int(floor(log2(nProcsPerGroup)));
 
     // The depth of a node n is the length of the path from the root to the node.
     // The depth of the root node is zero. For example:
@@ -159,33 +225,37 @@ void MEParallel::reduceNE(askap::scimath::INormalEquations::ShPtr ne)
     // floor(log2(1 + 1, 2)) == 1
     // floor(log2(2 + 1, 2)) == 1
     // floor(log2(3 + 1, 2)) == 2
-    const int depth = int(floor(log2(rank + 1)));
+    const int depth = int(floor(log2(rank)));
     ASKAPCHECK(depth <= height, "Depth exceeds height");
 
     // One reduction step is executed for each level of the tree
     // (except the top level)
-    for (int level = height; level > 0; --level) {
+    for (int level = height; level >= 0; --level) {
 
         // Only two levels participate in each step, upper level are receivers
         // and lower level are senders
         if (depth == level) {
             // This round I am a sender
-            const int parent = int(floor((rank - 1) / 2));
-            sendNormalEquations(ne, parent);
+            const int parent = int(floor((rank) / 2));
+            // the send and receive have to be true ranks - not ranks within the group
+            if (parent != 0)
+              sendNormalEquations(ne, parent + Group*nProcsPerGroup);
+            else
+              sendNormalEquations(ne, parent);
 
         } else if (depth == level - 1) {
             // This round I am a receiver
 
             // Receive from the left child if it exists
-            const int left = (2 * rank) + 1;
-            if (left < nProcs) {
-                ne->merge(*receiveNormalEquations(left));
+            const int left = (2 * rank);
+            if (left <= nProcsPerGroup) {
+                ne->merge(*receiveNormalEquations(left + Group*nProcsPerGroup));
             }
 
             // Receive from the right child if it exists
-            const int right = (2 * rank) + 2;
-            if (right < nProcs) {
-                ne->merge(*receiveNormalEquations(right));
+            const int right = (2 * rank) + 1;
+            if (right <= nProcsPerGroup) {
+                ne->merge(*receiveNormalEquations(right + Group*nProcsPerGroup));
             }
         } else {
             // This round I am a non-participant
@@ -199,7 +269,7 @@ void MEParallel::sendNormalEquations(const askap::scimath::INormalEquations::ShP
 
     casa::Timer timer;
     timer.mark();
-    ASKAPLOG_DEBUG_STR(logger, "Sending normal equations to rank " << dest);
+    ASKAPLOG_INFO_STR(logger, "Sending normal equations to rank " << dest);
 
     BlobOBufMW bobmw(itsComms, dest);
     LOFAR::BlobOStream out(bobmw);
@@ -207,7 +277,7 @@ void MEParallel::sendNormalEquations(const askap::scimath::INormalEquations::ShP
     out << itsComms.rank() << *ne;
     out.putEnd();
     bobmw.flush();
-    ASKAPLOG_DEBUG_STR(logger, "Sent normal equations to rank " << dest << " in "
+    ASKAPLOG_INFO_STR(logger, "Sent normal equations to rank " << dest << " in "
             << timer.real() << " seconds ");
 }
 
@@ -228,7 +298,7 @@ askap::scimath::INormalEquations::ShPtr MEParallel::receiveNormalEquations(int s
     } else if (dynamic_cast<GenericNormalEquations*>(itsNe.get())) {
         ne = GenericNormalEquations::ShPtr(new GenericNormalEquations());
     } else {
-        ne = itsNe->clone(); 
+        ne = itsNe->clone();
         ne->reset(); // Reset the normal equation, not the pointer!
     }
 
