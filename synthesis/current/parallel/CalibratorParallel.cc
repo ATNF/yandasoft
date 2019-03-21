7d3290ff11974116caf57537b4d51d4fe55fe744
VitaliyOgarko <VitaliyOgarko@csiro.au>
2019-03-21 08:03:16 +0000

    ASKAPSDP-3484 Comment added.

--- synthesis/current/parallel/CalibratorParallel.cc
+++ synthesis/current/parallel/CalibratorParallel.cc
@@ -37,6 +37,7 @@
 /// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ///
 /// @author Max Voronkov <maxim.voronkov@csiro.au>
+/// @author Vitaliy Ogarko <vogarko@gmail.com>
 ///
 
 #ifdef HAVE_MPI
