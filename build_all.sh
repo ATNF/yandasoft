1f63efed26ea60b1e4d32f546886cd8ea6f53441
Stephen Ord <steve.ord@gmail.com>
2019-07-04 08:47:15 +1000

    Added Selavy/Analysis

--- build_all.sh
+++ build_all.sh
@@ -435,6 +435,7 @@ if [ $install_askap_dependencies == yes ]; then
 	build_and_install https://bitbucket.csiro.au/scm/askapsdp/base-accessors.git master $yandasoft_opts
 	build_and_install https://bitbucket.csiro.au/scm/askapsdp/base-components.git master $yandasoft_opts
 	build_and_install https://bitbucket.csiro.au/scm/askapsdp/askap-pipelinetasks.git master $yandasoft_opts
+	build_and_install https://bitbucket.csiro.au/scm/askapsdp/askap-analysis.git master $yandasoft_opts
 
 	
 fi
