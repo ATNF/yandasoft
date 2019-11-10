752fe063d2a1c40b68281e3e90aa61225e2e25fb
Mark Wieringa <mark.wieringa@csiro.au>
2019-11-11 10:04:15 +1100

    Add parallactic angle rotation and X/Y swap

--- askap/gridding/TableVisGridder.h
+++ askap/gridding/TableVisGridder.h
@@ -160,6 +160,18 @@ namespace askap
       /// @param[in] flag new value of the flag
       void inline trackWeightPerPlane(const bool flag) { itsTrackWeightPerOversamplePlane = flag;}
 
+      /// @brief set or reset flag switching gridder to do Parallactic angle rotation
+      /// @details change itsPARotation
+      /// @param[in] flag new value of the flag
+      /// @param[in] if flag = true, use given angle + feed angle from feed table
+      void inline doPARotation(const bool flag, const double angle)
+      { itsPARotation = flag; itsPARotAngle = angle;}
+
+      /// @brief set or reset flag switching gridder to swap the polarisations
+      /// @details change itsSwapPols
+      /// @param[in] flag new value of the flag
+      void inline doSwapPols(const bool flag) { itsSwapPols = flag;}
+
       /// @brief set the largest angular separation between the pointing centre and the image centre
       /// @details If the threshold is positive, it is interpreted as the largest allowed angular
       /// separation between the beam (feed in the accessor terminology) pointing centre and the
@@ -552,6 +564,13 @@ protected:
 
       /// @brief true, if itsSumWeights tracks weights per oversampling plane
       bool itsTrackWeightPerOversamplePlane;
+
+      /// Are we doing Parallactic angle rotation?
+      bool itsPARotation;
+      double itsPARotAngle;
+
+      /// Are we swapping the polarisations?
+      bool itsSwapPols;
 
       /// @brief view of the currently used 2d grid
       /// @details to avoid creation/destruction overheads we keep a matrix that
