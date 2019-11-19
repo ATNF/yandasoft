f85e99c5f9d253ae391affa4189f9780dc4b1d64
Ubuntu <yanda@jenkins.ubfzha4wdtpe1oa5tkrr2skk4a.px.internal.cloudapp.net>
2019-11-19 11:59:15 +0000

    updated the docker files

--- deploy/docker/make-images.sh
+++ deploy/docker/make-images.sh
@@ -1,6 +1,6 @@
 # Makes the base dependencies
-docker build -f 001-yandabase -t yandabase .
+docker build --no-cache -f 001-yandabase -t yandabase .
 # Makes yandasoft
-docker build -f 002-develop-yandasoft -t yandasoft .
+docker build --no-cache -f 002-develop-yandasoft -t yandasoft .
 # Makes a slim version using the build products
-docker build -f 003-yandaslim -t yandaslim .
+docker build --no-cache -f 003-yandaslim -t yandaslim .
