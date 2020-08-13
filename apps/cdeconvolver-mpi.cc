04b19ed786647282782c058c9cdc1dca096b1ae0
Stephen Ord <steve.ord@gmail.com>
2020-08-13 13:58:22 +1000

    Fixed the iteration through the planes. The MPI sends are not set up thought so it only works serially.

--- apps/cdeconvolver-mpi.cc
+++ apps/cdeconvolver-mpi.cc
@@ -81,19 +81,22 @@ class CdeconvolverApp : public askap::Application
     
         static void correctConvolution(casacore::Array<casacore::Double>&, int, int, bool);
     
-       
+        void doTheWork(const LOFAR::ParameterSet, casacore::Array<casacore::Complex> &buffer, casacore::Array<casacore::Float> &psfArray,casacore::Array<casacore::Complex> &pcfArray, casacore::Array<casacore::Float> &model, casacore::Array<casacore::Float> &dirty,casacore::Array<casacore::Float> &restored);
         
         virtual int run(int argc, char* argv[])
         {
             StatReporter stats;
 
             const LOFAR::ParameterSet subset(config().makeSubset("Cdeconvolver."));
-            const LOFAR::ParameterSet subset_wiener_precon(subset.makeSubset("preconditioner.Wiener."));
            
             // This class must have scope outside the main try/catch block
             askap::askapparallel::AskapParallel comms(argc, const_cast<const char**>(argv));
             
             ASKAPLOG_INFO_STR(logger, "ASKAP image (MPI) deconvolver " << ASKAP_PACKAGE_VERSION);
+            
+            ASKAPCHECK(comms.nProcs() == 1,"Currently only SERIAL mode supported");
+                
+            
             // Need some metadata for the output cube constructions
             
             // Lets get the grid,pcf and psf cube names from the parset
@@ -107,7 +110,6 @@ class CdeconvolverApp : public askap::Application
             const std::string outResidCubeName = subset.getString("residual","residual");
             const std::string outRestoredCubeName = subset.getString("restored","restored");
             
-           
             
             // Lets load in a cube
             casacore::PagedImage<casacore::Complex> grid(gridCubeName);
@@ -119,8 +121,15 @@ class CdeconvolverApp : public askap::Application
             casa::IPosition blc(shape.nelements(),0);
             casa::IPosition trc(shape);
             int nchanCube = trc[3];
+            
+            // WorkArrays
+            casacore::Array<casacore::Float> psfArray;
+            casacore::Array<casacore::Complex> pcfArray;
+            casacore::Array<casacore::Complex> buffer;
+            
                            
             if (comms.isMaster()) { // only the master makes the output
+                
                 Int pixelAxis,worldAxis,coordinate;
                 CoordinateUtil::findSpectralAxis(pixelAxis,worldAxis,coordinate,grid.coordinates());
                 const SpectralCoordinate &sc = grid.coordinates().spectralCoordinate(coordinate);
@@ -143,166 +152,186 @@ class CdeconvolverApp : public askap::Application
                 itsResidualCube.reset(new askap::cp::CubeBuilder<casacore::Float>(subset, nchanCube, f0, cdelt,outResidCubeName));
                 itsRestoredCube.reset(new askap::cp::CubeBuilder<casacore::Float>(subset, nchanCube, f0, cdelt,outRestoredCubeName));
                 
-            }
             
-              
-            
-            
-            // lets calculate the allocations ...
+                // lets calculate the allocations ...
 
-           
             
-            vector<IPosition> inShapeVec;
-            vector<CoordinateSystem> inCoordSysVec;
-            // What fraction of the full problem does a rank have
-            // THe units here are channels - not polarisations.
-            int myFullAllocationSize = 0;
-            int myFullAllocationStart = 0;
-            int myFullAllocationStop = 0;
-            // Where a rank is in its allocation
-            int myAllocationSize = 1;
-            int myAllocationStart = 0;
-            int myAllocationStop = myAllocationStart + myAllocationSize;
+                vector<IPosition> inShapeVec;
+                vector<CoordinateSystem> inCoordSysVec;
+                // What fraction of the full problem does a rank have
+                // THe units here are channels - not polarisations.
+                int myFullAllocationSize = 0;
+                int myFullAllocationStart = 0;
+                int myFullAllocationStop = 0;
+                // Where a rank is in its allocation
+                int myAllocationSize = 1;
+                int myAllocationStart = 0;
+                int myAllocationStop = myAllocationStart + myAllocationSize;
             
-            if (nchanCube % comms.nProcs() != 0) {
+                if (nchanCube % comms.nProcs() != 0) {
                     ASKAPLOG_WARN_STR(logger,"Unbalanced allocation: num of ranks:" << comms.nProcs() << " not a factor of number of channels: "<< nchanCube);
-            }
-            
-            if (comms.nProcs() >= nchanCube) {
-                myFullAllocationSize = 1;
-            }
-            else {
-                myFullAllocationSize = nchanCube/comms.nProcs();
-            }
-                
-                    
-            myFullAllocationStart = comms.rank()*myFullAllocationSize;
-            myFullAllocationStop = myFullAllocationStart + myFullAllocationSize;
-
-            // unless last rank
-            if (comms.rank() == comms.nProcs()-1) {
-                myFullAllocationSize = trc[3] - myFullAllocationStart; // we are using End is Last
-            }
-            
-            ASKAPLOG_INFO_STR(logger,"RankAllocation starts at " << myFullAllocationStart << " and is " << myFullAllocationSize << " in size");
-            
-            int support = 3;
-            int alpha = 1;
-              // this is private to an inherited class so have to make a new one
-                    
-            for (myAllocationStart = myFullAllocationStart; myAllocationStart < myFullAllocationStop; myAllocationStart = myAllocationStart +  myAllocationSize) {
-                    
-                ASKAPLOG_INFO_STR(logger,"Input image shape " << shape);
-                ASKAPLOG_INFO_STR(logger,"Processing Channel " << myAllocationStart);
-                    
-                casa::IPosition inblc(shape.nelements(),0); // input bottom left corner of this allocation
-                casa::IPosition intrc(shape); // get the top right
-                myAllocationStop = myAllocationStart + myAllocationSize;
-                
-                inblc[3] = myAllocationStart;
-                intrc[0] = intrc[0]-1;
-                intrc[1] = intrc[1]-1;
-                intrc[2] = intrc[2]-1;
-                intrc[3] = myAllocationStart + myAllocationSize-1;
-                    
-                const casacore::Slicer slicer(inblc, intrc, casacore::Slicer::endIsLast);
-                ASKAPLOG_INFO_STR(logger,"Slicer is " << slicer);
-                
-                casacore::Array<casacore::Float> psfArray;
-                psf.getSlice(psfArray,slicer);
-                               
-                casacore::Array<casacore::Complex> pcfArray;
-                pcf.getSlice(pcfArray,slicer);
-                    
-                casacore::Array<casacore::Complex> buffer;
-                grid.doGetSlice(buffer, slicer);
-                
-                double norm = buffer.nelements()/max(psfArray);
-                
-                askap::scimath::fft2d(buffer,false);
-                buffer = buffer * norm;
-                ASKAPLOG_INFO_STR(logger,"Normalisation Factor is " << norm);
-                
-                casacore::Array<casacore::Double> dBuffer(buffer.shape());
-                casacore::Array<casacore::Float> fBuffer(buffer.shape());
-                casacore::convertArray<casacore::Double, casacore::Float> (dBuffer,real(buffer));
-
-                // Convolution correction probably should pull the support from the PARSET
-                correctConvolution(dBuffer,support,alpha,true);
-                casacore::convertArray<casacore::Float, casacore::Double>(fBuffer,dBuffer);
-            
-                // Preconditioning Assuming they only want Wiener.
-                boost::shared_ptr<WienerPreconditioner> wp = WienerPreconditioner::createPreconditioner(subset_wiener_precon);
-                ASKAPASSERT(wp);
-                
-                // The preconditioner assumes that the PCF is accumulated in the image domain so FFT it.
-                // I'm not normalising it as I dont think I need to.
-                
-                askap::scimath::fft2d(pcfArray,false);
-                casacore::Array<casacore::Float> pcfReal = real(pcfArray);
-                
-                wp->doPreconditioning(psfArray,fBuffer,pcfReal);
-                
-                // Now Deconvolution
-                // We have a normalised corrected preconditioned image
-                DeconvolverBase<Float, Complex>::ShPtr deconvolver;
-                string algorithm = subset.getString("solver.Clean.algorithm", "Basisfunction");
-
-                if (algorithm == "Basisfunction") {
-                    ASKAPLOG_INFO_STR(logger, "Constructing Basisfunction Clean solver");
-                    deconvolver.reset(new DeconvolverBasisFunction<Float, Complex>(fBuffer, psfArray));
-                    ASKAPASSERT(deconvolver);
-                } else if (algorithm == "MultiTermBasisfunction") {
-                    ASKAPLOG_INFO_STR(logger, "Constructing MultiTermBasisfunction Clean solver");
-                    deconvolver.reset(new DeconvolverMultiTermBasisFunction<Float, Complex>(fBuffer, psfArray));
-                    ASKAPASSERT(deconvolver);
-                } else if (algorithm == "Hogbom") {
-                    ASKAPLOG_INFO_STR(logger, "Constructing Hogbom Clean deconvolver");
-                    deconvolver.reset(new DeconvolverHogbom<Float, Complex>(fBuffer, psfArray));
-                    ASKAPASSERT(deconvolver);
-                } else {
-                    ASKAPTHROW(AskapError, "Unknown Clean algorithm " << algorithm);
                 }
-                deconvolver->configure(subset.makeSubset("solver.Clean."));
-              
-                deconvolver->deconvolve();
-                
-                // Now we need to deconvolve
-                itsModelCube->writeSlice(deconvolver->model(),myAllocationStart);
-                itsResidualCube->writeSlice(deconvolver->dirty(),myAllocationStart);
-                casacore::Vector<casacore::Array<casacore::Float> > restored(1);
-                
-                if(deconvolver->restore(restored))
-                    itsRestoredCube->writeSlice(restored(0),myAllocationStart);
-                
-                
             
+                if (comms.nProcs() >= nchanCube) {
+                    myFullAllocationSize = 1;
+                }
+                else {
+                    myFullAllocationSize = nchanCube/comms.nProcs();
+                }
+                
+                // lets loop over ranks
+                for (int theRank = 0; theRank < comms.nProcs(); theRank++) {
+                    
+                    myFullAllocationStart = theRank*myFullAllocationSize;
+                    myFullAllocationStop = myFullAllocationStart + myFullAllocationSize;
+
+                    // unless last rank
+                    if (theRank == comms.nProcs()-1) {
+                        myFullAllocationSize = trc[3] - myFullAllocationStart; // we are using End is Last
+                    }
+            
+                    ASKAPLOG_INFO_STR(logger,"Rank " << theRank << " - RankAllocation starts at " << myFullAllocationStart << " and is " << myFullAllocationSize << " in size");
+            
+                    // this is private to an inherited class so have to make a new one
+                    
+                    for (myAllocationStart = myFullAllocationStart; myAllocationStart < myFullAllocationStop; myAllocationStart = myAllocationStart +  myAllocationSize) {
+                    
+                        ASKAPLOG_INFO_STR(logger,"Input image shape " << shape);
+                        ASKAPLOG_INFO_STR(logger,"Processing Channel " << myAllocationStart);
+                    
+                        casa::IPosition inblc(shape.nelements(),0); // input bottom left corner of this allocation
+                        casa::IPosition intrc(shape); // get the top right
+                        myAllocationStop = myAllocationStart + myAllocationSize;
+                
+                        inblc[3] = myAllocationStart;
+                        intrc[0] = intrc[0]-1;
+                        intrc[1] = intrc[1]-1;
+                        intrc[2] = intrc[2]-1;
+                        intrc[3] = myAllocationStart + myAllocationSize-1;
+                    
+                        const casacore::Slicer slicer(inblc, intrc, casacore::Slicer::endIsLast);
+                        ASKAPLOG_INFO_STR(logger,"Slicer is " << slicer);
+                
+                        
+                        psf.getSlice(psfArray,slicer);
+                        pcf.getSlice(pcfArray,slicer);
+                        grid.doGetSlice(buffer, slicer);
+                        
+                        if (comms.nProcs() > 1) {
+                            ASKAPLOG_INFO_STR(logger, "Sending Allocation to Worker");
+                        }
+                        else {
+                            // do the work
+                            scimath::MultiDimArrayPlaneIter planeIter(buffer.shape());
+                            
+                            
+                            for (; planeIter.hasMore(); planeIter.next()) { // this is a loop over the polarisations as well as channels
+                                
+                                casacore::IPosition curpos = planeIter.position();
+                                // the inputs
+                                casacore::Array<casacore::Complex> thisBuffer = planeIter.getPlane(buffer, curpos);
+                                casacore::Array<casacore::Complex> thisPCFBUffer = planeIter.getPlane(pcfArray, curpos);
+                                casacore::Array<casacore::Float> thisPSFBuffer = planeIter.getPlane(psfArray, curpos);
+                                
+                                
+                                // the outputs
+                                casacore::Array<casacore::Float> model;
+                                casacore::Array<casacore::Float> dirty;
+                                casacore::Array<casacore::Float> restored;
+                                
+                                
+                                doTheWork(subset, thisBuffer, thisPSFBuffer, thisPCFBUffer, model, dirty, restored);
+                                
+                                // write out the slice
+                                itsModelCube->writeSlice(model,myAllocationStart);
+                                itsResidualCube->writeSlice(dirty,myAllocationStart);
+                                itsRestoredCube->writeSlice(restored,myAllocationStart);
+                            }
+                        
+                            
+                        }
+                        
+                    }
+                
+                        
+                }
                 
             }
-
-            // output Model cube
-            // this->itsModelCube.reset(new askap::cp::CubeBuilder<casacore::Float>(subset, nchanCube, f0, freqinc, "model"));
-            // output Residual cube
-            // this->itsResidualCube.reset(new askap::cp::CubeBuilder<casacore::Float>(subset, nchanCube, f0, freqinc,"residual"));
-            // boost::shared_ptr<DeconvolverBase<casacore::Float, casacore::Complex> > deconvolver::make(subset));
-            
-            // deconvolver->deconvolve();
-
-            // Now write the model and residual to disk using the names specified in the 
-            // parset. We simply copy the dirty image and then write the array into 
-            // the resulting image. 
-            // DeconvolverHelpers::putArrayToImage(deconvolver->model(), "model", "dirty", subset);
-            // DeconvolverHelpers::putArrayToImage(deconvolver->dirty(), "residual", "dirty", subset);
-
-            // Vector<Array<float> > restored(1);
-            // if(deconvolver->restore(restored)) {
-            //    DeconvolverHelpers::putArrayToImage(restored(0), "restored", "dirty", subset);
-            // }
 
             stats.logSummary();
             return 0;
         }
 };
+            
+
+void CdeconvolverApp::doTheWork(const LOFAR::ParameterSet subset, casacore::Array<casacore::Complex> &buffer, casacore::Array<casacore::Float> &psfArray,casacore::Array<casacore::Complex> &pcfArray, casacore::Array<casacore::Float> &model, casacore::Array<casacore::Float> &dirty, casacore::Array<casacore::Float> &restored) {
+    
+    ASKAPLOG_INFO_STR(logger,"Array Shape: " << buffer.shape());
+    double norm = buffer.nelements()/max(psfArray);
+                
+    askap::scimath::fft2d(buffer,false);
+    buffer = buffer * norm;
+    ASKAPLOG_INFO_STR(logger,"Normalisation Factor is " << norm);
+    
+    casacore::Array<casacore::Double> dBuffer(buffer.shape());
+    casacore::Array<casacore::Float> fBuffer(buffer.shape());
+    casacore::convertArray<casacore::Double, casacore::Float> (dBuffer,real(buffer));
+    
+    // Convolution correction probably should pull the support from the PARSET
+    // alpha is usually 1
+    // support is ususally 3
+    int alpha = 1;
+    int support = 3;
+    correctConvolution(dBuffer,support,alpha,true);
+    
+    casacore::convertArray<casacore::Float, casacore::Double>(fBuffer,dBuffer);
+    
+    // Preconditioning Assuming they only want Wiener.
+    boost::shared_ptr<WienerPreconditioner> wp = WienerPreconditioner::createPreconditioner(subset.makeSubset("preconditioner.Wiener."));
+    ASKAPASSERT(wp);
+    
+    // The preconditioner assumes that the PCF is accumulated in the image domain so FFT it.
+    // I'm not normalising it as I dont think I need to.
+    
+    askap::scimath::fft2d(pcfArray,false);
+    casacore::Array<casacore::Float> pcfReal = real(pcfArray);
+    
+    wp->doPreconditioning(psfArray,fBuffer,pcfReal);
+    
+    // Now Deconvolution
+    // We have a normalised corrected preconditioned image
+    DeconvolverBase<Float, Complex>::ShPtr deconvolver;
+    string algorithm = subset.getString("solver.Clean.algorithm", "Basisfunction");
+    
+    if (algorithm == "Basisfunction") {
+        ASKAPLOG_INFO_STR(logger, "Constructing Basisfunction Clean solver");
+        deconvolver.reset(new DeconvolverBasisFunction<Float, Complex>(fBuffer, psfArray));
+        ASKAPASSERT(deconvolver);
+    } else if (algorithm == "MultiTermBasisfunction") {
+        ASKAPLOG_INFO_STR(logger, "Constructing MultiTermBasisfunction Clean solver");
+        deconvolver.reset(new DeconvolverMultiTermBasisFunction<Float, Complex>(fBuffer, psfArray));
+        ASKAPASSERT(deconvolver);
+    } else if (algorithm == "Hogbom") {
+        ASKAPLOG_INFO_STR(logger, "Constructing Hogbom Clean deconvolver");
+        deconvolver.reset(new DeconvolverHogbom<Float, Complex>(fBuffer, psfArray));
+        ASKAPASSERT(deconvolver);
+    } else {
+        ASKAPTHROW(AskapError, "Unknown Clean algorithm " << algorithm);
+    }
+    
+    deconvolver->configure(subset.makeSubset("solver.Clean."));
+    
+    deconvolver->deconvolve();
+    
+    model = deconvolver->model().copy();
+    dirty = deconvolver->dirty().copy();
+    
+    casacore::Vector< casacore::Array<casacore::Float> > restored_vec(1); // some times there are more that one restore
+    if(deconvolver->restore(restored_vec))
+        restored = restored_vec(0).copy();
+    
+}
+
 void CdeconvolverApp::correctConvolution(casacore::Array<double>& grid, int support=3, int alpha = 1, bool itsInterp = true)
 {
     casacore::IPosition itsShape = grid.shape();
@@ -400,6 +429,7 @@ void CdeconvolverApp::correctConvolution(casacore::Array<double>& grid, int supp
 
     
 }
+
 // Main function
 int main(int argc, char* argv[])
 {
