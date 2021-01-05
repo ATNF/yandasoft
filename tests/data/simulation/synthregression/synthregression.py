ce0be4becc1eb2eafda738a8e57c03f0d8875a27
Stephen Ord <steve.ord@gmail.com>
2021-01-05 12:57:54 +1100

    Forgot to actaully update synregression.py itself
    Signed-off-by: Stephen Ord <steve.ord@gmail.com>

--- tests/data/simulation/synthregression/synthregression.py
+++ tests/data/simulation/synthregression/synthregression.py
@@ -1,16 +1,16 @@
-print "wtermtest: WProject and WStack gridders"
+print("wtermtest: WProject and WStack gridders")
 import wtermtest
-print "pbcorrtest: single field with mosaicing gridders (primary beam correction)"
+print("pbcorrtest: single field with mosaicing gridders (primary beam correction)")
 import pbcorrtest
-print "noisetest: testing that the noise in the output image is as expected"
+print("noisetest: testing that the noise in the output image is as expected")
 import noisetest
-print "facetingtest: test of faceted imaging with spherical function gridder"
+print("facetingtest: test of faceted imaging with spherical function gridder")
 import facetingtest
-print "calibratortest: test of ccalibrator"
+print("calibratortest: test of ccalibrator")
 import calibratortest
-print "leakagecalibtest: test of polarisation leakage calibration"
+print("leakagecalibtest: test of polarisation leakage calibration")
 import leakagecalibtest
-print "1934-638: test source position and flux on real ATCA data"
+print("1934-638: test source position and flux on real ATCA data")
 import test1934
-print "msmfs test with the new imager"
+print("msmfs test with the new imager")
 import testmsmfs
