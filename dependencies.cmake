dcdef328426c9636321d0a28a46d4ed65395321e
Eric Bastholm <eric.bastholm@csiro.au>
2021-12-09 11:39:57 +0800

    AXA-1508 Depencency version changes - lofar-common -> 1.2.0, lofar-blob -> 1.2.0, base-askap -> 1.3.0, base-imagemath -> 1.4.0, base-scimath -> 1.4.0, base-askapparallel -> 1.2.0, base-accessors -> 1.4.0, base-logfilters -> 1.2.0.

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,18 +11,12 @@
 
 #     Identifier                   tag/branch       cache  type      description                        force it
 set ( ASKAP_CMAKE_TAG              tags/1.2.0                               CACHE  STRING    "askap.cmake tools"                FORCE )
-set ( LOFAR_COMMON_TAG             tags/1.1.0                               CACHE  STRING    "lofar-common version"             FORCE )
-set ( LOFAR_BLOB_TAG               tags/1.1.0                               CACHE  STRING    "lofar-blob version"               FORCE )
-# need to release new base-askap, this tag is the state after pull request #37
-set ( BASE_ASKAP_TAG               30088d444dcdacc4d4c896a1cf6ad3cd0b1d6a97   CACHE  STRING    "base-askap version"               FORCE )
-set ( BASE_LOGFILTERS_TAG          tags/1.1.0                               CACHE  STRING    "base-logfilters version"          FORCE )
-# need to release new imagemath, this tag is the state after pull request #38 
-set ( BASE_IMAGEMATH_TAG           483d77c96067f726e7078e7ec6799a39830d269a   CACHE  STRING    "base-imagemath version"           FORCE )
-# need to release new base-parallel, this tag is the state after pull request #14
-set ( BASE_ASKAPPARRALLEL_TAG      afd594d5906c24043dd10d4e8472b282fd10e289   CACHE  STRING    "base-askapparrallel version"      FORCE )
-# need to release new scimath, this tag is the state after pull request #49
-set ( BASE_SCIMATH_TAG             96404d4d75d2bc7dcaf7fa8ddf462f64f91e7488   CACHE  STRING    "base-scimath version"             FORCE )
-# need to release new accessors, this tag is the state after pull request #39
-set ( BASE_ACCESSORS_TAG           083682416cf7d1604688fa33f5f19e5ad6e70e98   CACHE  STRING    "base-accessors version"           FORCE )
-set ( BASE_COMPONENTS_TAG          tags/1.1.0                               CACHE  STRING    "base-components version"          FORCE )
- 
+set ( LOFAR_COMMON_TAG             tags/1.2.0                               CACHE  STRING    "lofar-common version"             FORCE )
+set ( LOFAR_BLOB_TAG               tags/1.2.0                               CACHE  STRING    "lofar-blob version"               FORCE )
+set ( BASE_ASKAP_TAG               tags/1.3.0                               CACHE  STRING    "base-askap version"               FORCE )
+set ( BASE_LOGFILTERS_TAG          tags/1.2.0                               CACHE  STRING    "base-logfilters version"          FORCE )
+set ( BASE_IMAGEMATH_TAG           tags/1.4.0                               CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_ASKAPPARRALLEL_TAG      tags/1.2.0                               CACHE  STRING    "base-askapparrallel version"      FORCE )
+set ( BASE_SCIMATH_TAG             tags/1.4.0                               CACHE  STRING    "base-scimath version"             FORCE )
+set ( BASE_ACCESSORS_TAG           tags/1.4.0                               CACHE  STRING    "base-accessors version"           FORCE )
+set ( BASE_COMPONENTS_TAG          tags/1.2.0                               CACHE  STRING    "base-components version"          FORCE )
