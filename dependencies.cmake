867eafefbb421399a1160e66b1b7441301e39f1d
Max Voronkov <Maxim.Voronkov@csiro.au>
2022-02-23 10:45:46 +0800

    Trying to fix CI for the casacore-3.4 branch (AXA-689)

--- dependencies.cmake
+++ dependencies.cmake
@@ -10,13 +10,15 @@
 # repo.
 
 #     Identifier                   tag/branch       cache  type      description                        force it
-set ( ASKAP_CMAKE_TAG              tags/1.2.0                               CACHE  STRING    "askap.cmake tools"                FORCE )
+# the state of develop branch after PR 29 (AXA-1636) has been merged
+set ( ASKAP_CMAKE_TAG              30a2e560c549865aeb4743df1ae6f793c5ef656d CACHE  STRING    "askap.cmake tools"                FORCE )
 set ( LOFAR_COMMON_TAG             tags/1.2.0                               CACHE  STRING    "lofar-common version"             FORCE )
 set ( LOFAR_BLOB_TAG               tags/1.2.0                               CACHE  STRING    "lofar-blob version"               FORCE )
 set ( BASE_ASKAP_TAG               tags/1.3.0                               CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_LOGFILTERS_TAG          tags/1.2.0                               CACHE  STRING    "base-logfilters version"          FORCE )
-set ( BASE_IMAGEMATH_TAG           tags/1.4.0                               CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_IMAGEMATH_TAG           feature/AXA-689-casacore-3.4-transition       CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARRALLEL_TAG      tags/1.2.0                               CACHE  STRING    "base-askapparrallel version"      FORCE )
-set ( BASE_SCIMATH_TAG             tags/1.4.0                               CACHE  STRING    "base-scimath version"             FORCE )
-set ( BASE_ACCESSORS_TAG           tags/1.4.0                               CACHE  STRING    "base-accessors version"           FORCE )
+# the state of develop branch after PR 52 has been merged (AXA-689 changes compatible with the old casacore, hence merged)
+set ( BASE_SCIMATH_TAG             359a814402b8d70ab70b3387c26eba89fa86be87 CACHE  STRING    "base-scimath version"             FORCE )
+set ( BASE_ACCESSORS_TAG           feature/AXA-689-casacore-3.4-transition       CACHE  STRING    "base-accessors version"           FORCE )
 set ( BASE_COMPONENTS_TAG          tags/1.2.0                               CACHE  STRING    "base-components version"          FORCE )
