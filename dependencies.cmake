f642c5ba26511fe1917ae9366472affd360be399
Max Voronkov <Maxim.Voronkov@csiro.au>
2022-06-02 11:59:59 +0800

    Updated dependency to the recently released base-scimath-1.6.0 (AXA-1031)

--- dependencies.cmake
+++ dependencies.cmake
@@ -17,7 +17,7 @@ set ( BASE_ASKAP_TAG               tags/1.4.0       CACHE  STRING    "base-askap
 set ( BASE_LOGFILTERS_TAG          tags/1.2.0       CACHE  STRING    "base-logfilters version"          FORCE )
 #set ( BASE_IMAGEMATH_TAG           tags/1.5.1       CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARRALLEL_TAG      tags/1.2.0       CACHE  STRING    "base-askapparrallel version"      FORCE )
-set ( BASE_SCIMATH_TAG             tags/1.5.1       CACHE  STRING    "base-scimath version"             FORCE )
+set ( BASE_SCIMATH_TAG             tags/1.6.0       CACHE  STRING    "base-scimath version"             FORCE )
 #set ( BASE_ACCESSORS_TAG           tags/1.5.1       CACHE  STRING    "base-accessors version"           FORCE )
 set ( BASE_COMPONENTS_TAG          tags/1.3.0       CACHE  STRING    "base-components version"          FORCE )
 
