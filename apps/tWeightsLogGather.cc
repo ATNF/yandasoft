dac80311e09737f10acef2be66756a6cf41f8510
Max Voronkov <Maxim.Voronkov@csiro.au>
2022-07-26 14:46:49 +0800

    fully functional tool, ready for debugging (AXA-1951)

--- apps/tWeightsLogGather.cc
+++ apps/tWeightsLogGather.cc
@@ -3,6 +3,20 @@
 ///          but in isolation. A number of parset parameters understood by imager are also understood by this tool
 ///          to setup communication classes the same way (whether it has any effect on it or not)
 ///
+///    Parameters of the parset file:
+///    # standard options which affect general setup of the distribution (as with imager)
+///    nwriters = 1
+///    nchanpercore = 1
+///    singleoutputfile = false
+///    # default delay before gather operation is called in seconds (set to 0 for no delay)
+///    delay.default = 10
+///    # delay for rank XX in seconds, overrides delay.default (set to 0 for no delay, omit the parameter to use the default one)
+///    delay.rankXX = 0
+///    # number of times the test is run
+///    ncycles = 1
+///    # if true, there is a barrier before the each cycle
+///    cyclebarrier = false
+///
 /// @copyright (c) 2009,2016 CSIRO
 /// Australia Telescope National Facility (ATNF)
 /// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
@@ -32,6 +46,7 @@
 
 // System includes
 #include <string>
+#include <map>
 
 // ASKAPsoft includes
 #include <askap/askap/Application.h>
@@ -39,6 +54,7 @@
 #include <askap/askap/AskapError.h>
 #include <askap/askap/StatReporter.h>
 #include <Common/ParameterSet.h>
+#include <askap/imageaccess/WeightsLog.h>
 
 // Local Package includes
 #include "askap/distributedimager/CubeComms.h"
@@ -51,6 +67,61 @@ ASKAP_LOGGER(logger, ".tWeightsLogGather");
 class WeightsLogGatherTestApp : public askap::Application
 {
     public:
+        void runActualTest(askap::cp::CubeComms &comms) {
+             const int nwriters = config().getInt32("nwriters",1);
+             ASKAPCHECK(nwriters>0,"Number of writers must be greater than 0");
+
+             askap::accessors::WeightsLog weightslog;
+             ASKAPLOG_INFO_STR(logger, "About to add weights list of size " << itsWeightsList.size() << " to the weights logger");
+             weightslog.weightslist() = itsWeightsList;
+
+             if (nwriters > 1 && config().getBool("singleoutputfile",false)) {
+                 std::list<int> creators = comms.getCubeCreators();
+                 ASKAPASSERT(creators.size() == 1);
+                 const int creatorRank = creators.front();
+                 ASKAPLOG_INFO_STR(logger, "Gathering all weights information, creator is rank " << creatorRank);
+                 weightslog.gather(comms, creatorRank, false);
+             }
+             ASKAPLOG_INFO_STR(logger, "Finished the test, weights list has "<<itsWeightsList.size()<<" elements");
+        }
+
+        void prepareSomeWeightsList(askap::cp::CubeComms &comms) {
+ 
+             // do the same math & logging as the imager worker 
+             // although we don't necessarily need this information, it may be handy 
+             // to be able to fill the list to test depending on this info
+             const int nchanpercore = config().getInt32("nchanpercore", 1);
+
+             // lets calculate a base
+             const unsigned int nWorkers = comms.nProcs() - 1;
+             const unsigned int nWorkersPerGroup = nWorkers / comms.nGroups();
+             ASKAPCHECK(nWorkersPerGroup > 0, "Expect at least one worker per group, nWorkers="<<nWorkers<<" number of groups: "<<comms.nGroups());
+
+             const unsigned int id = comms.rank();
+             // e. g. rank 8, 3 per group should be pos. 1 (zero index)
+             unsigned int posInGroup = (id % nWorkersPerGroup);
+
+             if (posInGroup == 0) {
+                 posInGroup = nWorkersPerGroup;
+             }
+             posInGroup = posInGroup - 1;
+
+             const unsigned int baseChannel = posInGroup * nchanpercore;
+             ASKAPLOG_INFO_STR(logger, "Distribution: Id " << id << " nWorkers " << nWorkers << " nGroups " << comms.nGroups()<<
+                               " base channel " << baseChannel << " PosInGrp " << posInGroup);
+             // now fill some weights
+             for (unsigned int chan = 0; chan < nchanpercore; ++chan) {
+                  itsWeightsList[chan+baseChannel] = 0.1 * id;
+             }
+             const unsigned int defaultDelay = config().getUint32("delay.default",10);
+             const std::string delayParamStr = "delay.rank"+utility::toString(id);
+             const unsigned int thisRankDelay = config().isDefined(delayParamStr) ? config().getUint32(delayParamStr) : defaultDelay;
+             if (thisRankDelay > 0) {
+                 ASKAPLOG_INFO_STR(logger, "Pausing for "<<thisRankDelay<<" seconds");
+                 sleep(thisRankDelay);
+             }
+        }
+
         virtual int run(int argc, char* argv[])
         {
             // Instantiate the comms class
@@ -70,7 +141,25 @@ class WeightsLogGatherTestApp : public askap::Application
                     ASKAPLOG_INFO_STR(logger, "All workers are treated as identical");
                 }
 
-                // setup and run the pattern
+                // this happens inside the ContinuumImager class
+                comms.buildCommIndex();
+
+                // setup and run the pattern in workers
+                const unsigned int ncycles = config().getUint32("ncycles",1);
+                ASKAPCHECK(ncycles > 0, "Number of cycles should be at least one");
+                const bool doBarrier = config().getBool("cyclebarrier",false);
+                for (unsigned int cycle = 0; cycle < ncycles; ++cycle) {
+                     ASKAPLOG_INFO_STR(logger, "Starting cycle "<<cycle + 1);
+                     itsWeightsList.clear();
+                     if (doBarrier) {
+                         ASKAPLOG_INFO_STR(logger, "Barrier before proceeding to the test");
+                         comms.barrier();
+                     }
+                     if (comms.isWorker()) {
+                         prepareSomeWeightsList(comms);
+                         runActualTest(comms);
+                    }
+                }
 
                 ASKAPLOG_INFO_STR(logger, "Reached the barrier at the end (not sure if it is needed, but the imager has it)");
                 comms.barrier(); 
@@ -95,6 +184,9 @@ class WeightsLogGatherTestApp : public askap::Application
             const std::string pkgVersion = std::string("yandasoft:") + ASKAP_PACKAGE_VERSION;
             return pkgVersion;
         }
+
+        /// @brief weights list to work with
+        std::map<unsigned int, float> itsWeightsList;
 };
 
 int main(int argc, char *argv[])
