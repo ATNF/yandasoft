118aadcc5cdf7bad67eef940ba1974b90651a664
Eric Bastholm <eric.bastholm@csiro.au>
2022-08-02 15:47:51 +0800

    AXA-1970 updated depdendencies.cmake for base-imagemath hotfix 1.6.1 impacts.

--- dependencies.cmake
+++ dependencies.cmake
@@ -15,11 +15,11 @@ set ( LOFAR_COMMON_TAG             tags/1.2.0       CACHE  STRING    "lofar-comm
 set ( LOFAR_BLOB_TAG               tags/1.3.0       CACHE  STRING    "lofar-blob version"               FORCE )
 set ( BASE_ASKAP_TAG               tags/1.5.0       CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_LOGFILTERS_TAG          tags/1.3.0       CACHE  STRING    "base-logfilters version"          FORCE )
-set ( BASE_IMAGEMATH_TAG           tags/1.6.0       CACHE  STRING    "base-imagemath version"           FORCE )
-set ( BASE_ASKAPPARRALLEL_TAG      tags/1.3.0       CACHE  STRING    "base-askapparrallel version"      FORCE )
-set ( BASE_SCIMATH_TAG             tags/1.7.0       CACHE  STRING    "base-scimath version"             FORCE )
-set ( BASE_ACCESSORS_TAG           tags/1.6.0       CACHE  STRING    "base-accessors version"           FORCE )
-set ( BASE_COMPONENTS_TAG          tags/1.4.0       CACHE  STRING    "base-components version"          FORCE )
+set ( BASE_IMAGEMATH_TAG           tags/1.6.1       CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_ASKAPPARRALLEL_TAG      tags/1.3.1       CACHE  STRING    "base-askapparrallel version"      FORCE )
+set ( BASE_SCIMATH_TAG             tags/1.7.1       CACHE  STRING    "base-scimath version"             FORCE )
+set ( BASE_ACCESSORS_TAG           tags/1.6.1       CACHE  STRING    "base-accessors version"           FORCE )
+set ( BASE_COMPONENTS_TAG          tags/1.4.1       CACHE  STRING    "base-components version"          FORCE )
 
 # develop - Use these entries for develop until we fix with AXA-1719
 #set ( ASKAP_CMAKE_TAG              develop          CACHE  STRING    "askap.cmake tools"                FORCE )
