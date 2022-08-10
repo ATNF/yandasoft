b781c552c30de6384113357800c04212c63baa5c
Eric Bastholm <eric.bastholm@csiro.au>
2022-08-10 19:21:19 +0800

    AXA-1989 Updated dependency base-imagemath to 1.7.0.

--- dependencies.cmake
+++ dependencies.cmake
@@ -15,7 +15,7 @@ set ( LOFAR_COMMON_TAG             tags/1.2.0       CACHE  STRING    "lofar-comm
 set ( LOFAR_BLOB_TAG               tags/1.3.0       CACHE  STRING    "lofar-blob version"               FORCE )
 set ( BASE_ASKAP_TAG               tags/1.5.0       CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_LOGFILTERS_TAG          tags/1.3.0       CACHE  STRING    "base-logfilters version"          FORCE )
-set ( BASE_IMAGEMATH_TAG           tags/1.6.1       CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_IMAGEMATH_TAG           tags/1.7.0       CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARRALLEL_TAG      tags/1.3.1       CACHE  STRING    "base-askapparrallel version"      FORCE )
 set ( BASE_SCIMATH_TAG             tags/1.7.1       CACHE  STRING    "base-scimath version"             FORCE )
 set ( BASE_ACCESSORS_TAG           tags/1.6.1       CACHE  STRING    "base-accessors version"           FORCE )
